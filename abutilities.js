594e6c28fa1ac28f62cc002c9586735e81d9fe38
Ariel <borochov@student.american.edu>
2013-11-22 23:42:00 -0500

    adding some event handlers

--- abutilities.js
+++ abutilities.js
@@ -1,3 +1,5 @@
+
+
 function buildMonthNameModule() {
   var names = ["January", "February", "March", "April",
                "May", "June", "July", "August", "September",
@@ -73,7 +75,131 @@ function parseINI( string) {
 			currentCategory.fields.push({ name: match[ 1], value: match[ 2]}); 
 			else throw new Error(" Line '" + line + "' is invalid."); 
 			}); 
-			return categories; 
-			}
+	return categories; 
+}
+function dom(name, attributes /*, children...*/) { 
+	console.log("inside dom");
+	var node = document.createElement(name); 
+	console.log("after var");
+	if (attributes) { forEachIn( attributes, function(name, value) { 
+		node.setAttribute(name,value); }); 
+		} 
+		for (var i = 2; i < arguments.length; i ++) { 
+			var child = arguments[ i]; 
+			if (typeof child == "string") child = document.createTextNode( child); 
+			node.appendChild( child); 
+		} 
+		console.log("return node");
+		return node; 
+}
+function forEach(array, action) {
+  for (var i = 0; i < array.length; i++)
+    action(array[i]);
+}
+function main()
+{
+	console.log("inside main");
+	 
+print("I love you cutie");
+
+console.log("after output");
+}
+
+var output = dom("DIV", {id: "printOutput"}, dom("H1", null, "Print output:")); 
+document.body.appendChild(output);
+function print() { 
+	var result = []; 
+	forEach( arguments, function( arg){
+		result.push( String( arg));
+		}); 
+		output.appendChild( dom("PRE", null, result.join(""))); 
+		
+}
+
+function forEachIn(object, action) {
+  for (var property in object) {
+    if (object.hasOwnProperty(property))
+      action(property, object[property]);
+  }
+}
+function registerEventHandler( node, event, handler) { 
+	if (typeof node.addEventListener == "function") node.addEventListener( event, handler, false);
+	else node.attachEvent("on" + event, handler); 
+	}
+	
+	
+	function unregisterEventHandler( node, event, handler) { 
+		if (typeof node.removeEventListener == "function") node.removeEventListener( event, handler,false); 
+		else node.detachEvent(" on" + event, handler); 
+		}
+		registerEventHandler( document.body, "click", function( event) {
+			 event = event || window.event;
+			 print( event.clientX, ",", event.clientY); 
+			 });
+			 
+			 
+registerEventHandler( document.body, "keypress", function( event) { 
+	event = event || window.event; 
+	var charCode = event.charCode || event.keyCode; 
+	if (charCode) print("Character '", String.fromCharCode( charCode), "' was typed.");
+	 });
+
+function normalizeEvent( event) { 
+	if (! event.stopPropagation) { 
+		event.stopPropagation = function() {this.cancelBubble = true;}; 
+		event.preventDefault = function() {this.returnValue = false;}; 
+		} 
+		if (! event.stop) event.stop = function() { 
+			this.stopPropagation();this.preventDefault(); 
+			}; 
+			if (event.srcElement && !event.target) event.target = event.srcElement; 
+			if (( event.toElement || event.fromElement) && !event.relatedTarget) event.relatedTarget = event.toElement || event.fromElement; 
+			if (event.clientX != undefined && event.pageX == undefined) { 
+				event.pageX = event.clientX + document.body.scrollLeft; 
+				event.pageY = event.clientY + document.body.scrollTop; 
+				} if (event.type == "keypress") event.character = String.fromCharCode( event.charCode || event.keyCode); 
+				return event; 
+				}
+				
+function addHandler(node, type, handler) { 
+	function wrapHandler( event) { 
+		handler( normalizeEvent( event || window.event)); 
+		} 
+		registerEventHandler( node, type, wrapHandler); 
+		return {node: node, type: type, handler: wrapHandler}; 
+		} 
+function removeHandler( object) { 
+	unregisterEventHandler( object.node, object.type, object.handler); 
+	}
+addHandler( textfield, "focus", function( event) { event.target.style.backgroundColor = "yellow"; }); addHandler( textfield, "blur", function( event) { event.target.style.backgroundColor = ""; });
+addHandler( textfield, "change", function( event) { print(" Content of text field changed to '", event.target.value, "'."); });
+
+function Square( character, img) { 
+	this.img = img; 
+	var content = {"@": "player", "#": "wall", "*": "exit", " ": "empty", "0": "boulder"}[character]; 
+	if (content == null) throw new Error("Unrecognized character: '" + character + "'"); 
+	this.setContent( content); 
+}
+	Square.prototype.setContent = function(content) { this.content = content; this.img.src = "img/ sokoban/" + content + ". png"; };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
 
 
